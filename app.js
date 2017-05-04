c1fe156cda887cd3f91e83dee3ab97c834aac41b
mnaselli <marty.naselli@gmail.com>
2017-05-04 18:05:37 -0400

    stupid fucking leaflet shit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-L.Projection.NoWrap = {
+
+L.Projection.NoWrap = L.Util.extend({}, L.CRS.Simple.projection, {
   project: function (latlng) {
     return new L.Point(latlng.lng, latlng.lat);
   },
   unproject: function (point, unbounded) {
     return new L.LatLng(point.y, point.x, true);
   }
-};
+});
 L.CRS.Direct = L.Util.extend({}, L.CRS, {
   code: 'Direct',
   projection: L.Projection.NoWrap,
@@ -13,26 +14,28 @@ L.CRS.Direct = L.Util.extend({}, L.CRS, {
 });
 
 var coordBox = $(coords),
-    mapUrl = 'https://s3.amazonaws.com/arkmaptiles/{z}/{x}/{y}.png';
+    mapUrl = 'https://s3.amazonaws.com/arkmaptiles/{z}/{x}/{y}.png',
+		markers = [];
+
+var map = L.map('map',{
+    worldCopyJump: false, 
+    crs: L.CRS.Direct,
+    attributionControl: false,
+    zoomSnap: 0,
+    zoomDelta: 0.01,
+    minZoom: 1,
+    maxZoom: 6,
+  }).setView([0,0],2);
 
 var mapLayer = L.tileLayer(mapUrl, {
-      minZoom: 1,
-      maxZoom: 6,
-      tms: true,
-      continuousWorld: false,
-      noWrap: true,
-      detectRetina: true,
+    minZoom: 1,
+  tms: true,
+    maxZoom: 6,
+    noWrap: true,
+    crs: L.CRS.Direct,
+    continuousWorld: false,
       worldCopyJump: false, 
-      crs: L.CRS.Direct
-    });
-var map = L.map('map',{
-      layers: [mapLayer],
-      worldCopyJump: false, 
-      crs: L.CRS.Direct,
-      attributionControl: false,
-      zoomSnap: 0,
-      zoomDelta: 0.01,
-    }).setView([0,0],0);
+  }).addTo(map);
 
 function getLatLng(latlng){
   //                latitude 7.2 - 92.8        longitude 7.2 - 92.8
@@ -40,13 +43,21 @@ function getLatLng(latlng){
       lng = Number(Math.round(latlng.lng * 83 + 8.3+'e1')+'e-1');
 	return lat + ',' + lng;
 };
+function updateMarkers() {
+	for (var i=0; i<markers.length; i++) {
+    var marker = markers[i];
+    marker.setTooltipContent(getLatLng(marker._latlng));
+  }
+}
 
 map.on('mousemove', function(e) {
   coordBox.text(getLatLng(e.latlng));
 });
 
 map.on('click', function(e){
-  L.marker([e.latlng.lat, e.latlng.lng],{
-		title: getLatLng(e.latlng)
-  }).addTo(map);
+  var marker = L.marker([e.latlng.lat, e.latlng.lng], { draggable: true });
+  marker.on('moveend', updateMarkers)
+  marker.addTo(map)
+  marker.bindTooltip(getLatLng(marker._latlng)).openTooltip();
+  markers.push(marker);
 });
